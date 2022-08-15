d10244311d8240ae0d7b78549caf17b4acea5ebb
ducnm <nguyenminhduc1906@gmail.com>
2022-08-15 21:29:13 +0700

    fix(actions): recompile node action

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1,87 +1,81 @@
-/******/ (() => {
-  // webpackBootstrap
-  /******/ var __webpack_modules__ = {
-    /***/ 552: /***/ (module) => {
-      module.exports = eval("require")("@action/github");
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
-      /***/
-    },
+/***/ 552:
+/***/ ((module) => {
 
-    /***/ 396: /***/ (module) => {
-      module.exports = eval("require")("@actions/core");
+module.exports = eval("require")("@action/github");
 
-      /***/
-    },
 
-    /******/
-  };
-  /************************************************************************/
-  /******/ // The module cache
-  /******/ var __webpack_module_cache__ = {};
-  /******/
-  /******/ // The require function
-  /******/ function __nccwpck_require__(moduleId) {
-    /******/ // Check if module is in cache
-    /******/ var cachedModule = __webpack_module_cache__[moduleId];
-    /******/ if (cachedModule !== undefined) {
-      /******/ return cachedModule.exports;
-      /******/
-    }
-    /******/ // Create a new module (and put it into the cache)
-    /******/ var module = (__webpack_module_cache__[moduleId] = {
-      /******/ // no module.id needed
-      /******/ // no module.loaded needed
-      /******/ exports: {},
-      /******/
-    });
-    /******/
-    /******/ // Execute the module function
-    /******/ var threw = true;
-    /******/ try {
-      /******/ __webpack_modules__[moduleId](
-        module,
-        module.exports,
-        __nccwpck_require__
-      );
-      /******/ threw = false;
-      /******/
-    } finally {
-      /******/ if (threw) delete __webpack_module_cache__[moduleId];
-      /******/
-    }
-    /******/
-    /******/ // Return the exports of the module
-    /******/ return module.exports;
-    /******/
-  }
-  /******/
-  /************************************************************************/
-  /******/ /* webpack/runtime/compat */
-  /******/
-  /******/ if (typeof __nccwpck_require__ !== "undefined")
-    __nccwpck_require__.ab = __dirname + "/";
-  /******/
-  /************************************************************************/
-  var __webpack_exports__ = {};
-  // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-  (() => {
-    const core = __nccwpck_require__(396);
-    const github = __nccwpck_require__(552);
+/***/ }),
 
-    try {
-      // throw(new Error("Some error message"));
-      const name = core.getInput("who-to-greet");
-      console.log(`Hello ${name}`);
+/***/ 396:
+/***/ ((module) => {
 
-      const time = new Date();
-      core.setOutput("time", time.toTimeString());
+module.exports = eval("require")("@actions/core");
 
-      console.log(JSON.stringify(github, null, "\t"));
-    } catch (error) {
-      core.setFailed(error.message);
-    }
-  })();
 
-  module.exports = __webpack_exports__;
-  /******/
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(396);
+const github = __nccwpck_require__(552);
+
+try {
+  // throw(new Error("Some error message"));
+  const name = core.getInput("who-to-greet");
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
+  console.log(JSON.stringify(github, null, "\t"));
+} catch (error) {
+  core.setFailed(error.message);
+}
+
 })();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
