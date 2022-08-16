58b292c1cf6157ca2ccb85f4c5b7d2dfde538327
ducnm <nguyenminhduc1906@gmail.com>
2022-08-16 15:31:48 +0700

    fix: endable debug
    Enable Action debug

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -63,13 +63,23 @@ const github = __nccwpck_require__(552);
 
 try {
   // throw(new Error("Some error message"));
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error();
+  ("Error message");
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroups("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
